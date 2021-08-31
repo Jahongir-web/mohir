720394d59662610f58e6695f9e9fbba66d636c23
Jahongir-web <jahongirmh@gmail.com>
2021-08-31 14:47:33 +0500

    re google url

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ const GOOGLE_CLIENT_SECRET = 'lZ3qPQe9_T0rJEzexeCegav5'
 passport.use(new GoogleStrategy({
   clientID: GOOGLE_CLIENT_ID,
   clientSecret: GOOGLE_CLIENT_SECRET,
-  callbackURL: 'https://mohir-server.herokuapp.com/google'
+  callbackURL: '/google'
 }, (accessToken, refreshToken, profile, callback) => {
   callback(null, profile)
 }))
