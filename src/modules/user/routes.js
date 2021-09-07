41a6d93b4ee85876af1a2d628a54aa9e66f2ff84
Jahongir-web <jahongirmh@gmail.com>
2021-09-07 12:01:40 +0500

    55

--- src/modules/user/routes.js
+++ src/modules/user/routes.js
@@ -30,14 +30,12 @@ router.get('/google', passport.authenticate('google'), async(req, res) => {
     console.log(user);
     if(user){
       const accessToken = await sign(user)      
-      res.redirect('/')
-      res.send(accessToken)
+      res.send(accessToken).status(201)
     } else {
       const user = await model.signUpGoogle(data)
       if(user){
         const accessToken = await sign(user)      
-        res.redirect('/')
-        res.send(accessToken)
+        res.send(accessToken).status(201)
       }
     }
 
